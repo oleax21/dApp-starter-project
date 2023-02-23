4d529c2e679769cf22895a7e352c13f65bb50c79
shuhei <hello@shuhei.world>
2023-02-23 21:24:53 +0900

    feat: check wallet access

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-export default function App() {
-  const wave = () => {};
+const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount: ", currentAccount);
+
+  // window.ethereumにアクセスできることを確認
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+      // ユーザーのウォレットへのアクセスが許可されているかどうかを確認
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="mainContainer">
@@ -14,22 +42,22 @@ export default function App() {
           </span>{" "}
           WELCOME!
         </div>
-
         <div className="bio">
-          イーサリアムウォレットを接続して、メッセージを作成したら、
+          イーサリアムウォレットを接続して、「
           <span role="img" aria-label="hand-wave">
             👋
           </span>
-          を送ってください
+          (wave)」を送ってください
           <span role="img" aria-label="shine">
             ✨
           </span>
         </div>
-
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
-}
+};
+
+export default App;
